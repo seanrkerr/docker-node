edc608bf0b0e9e3df6168f37df7c405dab044efb
Sean Kerr <seankerr@Sean.local>
2016-06-11 08:31:19 +1000

    changes to proc file

--- server/lib/util.js
+++ server/lib/util.js
@@ -11,7 +11,6 @@
 export function toRes(res, status=200) {
 	return (err, thing) => {
 		if (err) return res.status(500).send(err);
-
 		if (thing && typeof thing.toObject==='function') {
 			thing = thing.toObject();
 		}
